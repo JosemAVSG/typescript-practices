edb208df02fc62659bd1eb6413561e8a6283e56b
Jose Miguel JGTECH <jmgg.95n@gmail.com>
2024-04-21 02:00:20 -0500

    adding interfaces

--- index.ts
+++ index.ts
@@ -1,6 +1,59 @@
-const a: Number = 1
-const b: Number = 2
+const a: number = 1
+const b: number = 2
 
-const sumar = (a: Number, b: Number) => a + b
+const sumar = (a: number, b: number) : number => a + b
 
 console.log(sumar(a, b));
+
+interface AudioBooks {
+    name:string,
+    duration:number,
+    author: {
+        name:string
+    }
+}
+interface Podcasts {
+    name:string,
+    duration:number,
+    author: {
+        name:string
+    }
+}
+interface Videos {
+    name:string,
+    duration:number,
+    author: {
+        name:string
+    }
+}
+
+interface PlayList{
+    name:string,
+    items: (AudioBooks | Podcasts | Videos)[]
+}
+
+const myPlaylist: PlayList = {
+    name: "My Playlist",
+    items: ([
+        {
+            name: "AudioBook",
+            duration: 100,
+            author: {
+                name: "Author 1"
+            }
+        },
+        {
+            name: "Podcast",
+            duration: 200,
+            author: {
+                name: "Author 2"
+            }
+        },
+        {
+            name: "Video",
+            duration: 300,
+            author: {
+                name: "Author 3"}
+        }
+    ])
+}
